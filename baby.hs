42afccc566979c2f7062f5eb525d5143370cc94d
Vitchyr Pong <vitchyr@gmail.com>
2016-06-02 18:11:50 -0400

    init commit with basic things

---
+++ baby.hs
@@ -0,0 +1,9 @@
+doubleMe x = x + x
+
+doubleUs x y = doubleMe x + doubleMe y
+
+doubleSmallNumber x = if x > 100
+                          then x
+                          else doubleMe x
+
+boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
